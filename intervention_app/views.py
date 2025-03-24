eef6a853fd1596e41e61829f2328ea6b946516de
Earnest Achayo <earnytechlive@gmail.com>
2025-03-25 00:10:11 +0300

    added login view logic

--- intervention_app/views.py
+++ intervention_app/views.py
@@ -1,6 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {})
+    # find out if logging in
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        #Authenticate user
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You have successfully logged in")
+            return redirect('home')
+        else:
+            messages.success(request, "Wrong username or password, please try again!")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+    
+def logout(request):
+    pass
+
+def register(request):
+    pass
+
