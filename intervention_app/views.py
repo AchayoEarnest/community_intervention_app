3a5f1da2308a63c1665ba7e3da3e90e95d2597f1
Earnest Achayo <earnytechlive@gmail.com>
2025-03-25 00:23:33 +0300

    added logout view and corresponding url link

--- intervention_app/views.py
+++ intervention_app/views.py
@@ -25,6 +25,7 @@ def home(request):
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out...")
+    return redirect('home')
 
 def register(request):
     pass
