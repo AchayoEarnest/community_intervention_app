44acc4f64e6309c4bae47392da953dec8eac81eb
Earnest Achayo <earnytechlive@gmail.com>
2025-03-25 00:16:07 +0300

    url link bug fixed-missing comma

--- intervention_app/urls.py
+++ intervention_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
-    path('logout/', views.logout_user, name='logout')
+    path('', views.home, name='home'),
+    path('logout/', views.logout_user, name='logout'),
 ]
