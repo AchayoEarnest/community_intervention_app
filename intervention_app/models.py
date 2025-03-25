05c15f13cf046d8a11c4b4b48cbc3d3c3491df16
Earnest Achayo <earnytechlive@gmail.com>
2025-03-25 22:36:52 +0300

    added intervention model and its corresponding fields

--- intervention_app/models.py
+++ intervention_app/models.py
@@ -16,10 +16,21 @@ class Enrollment(models.Model):
     guadian_phone_number = models.CharField(max_length=12)
     school= models.CharField(max_length=100)
     landmark= models.CharField(max_length=100)
-    age= models.CharField(max_length=100)
+    age = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name} ({self.age} years old)"
 
 
+class Intervention(models.Model):
+    enrollment = models.ForeignKey(Enrollment, on_delete=models.CASCADE)
+    intervention_name = models.CharField(max_length=100)
+    intervention_cartegory = models.CharField(max_length=100)
+    comments = models.TextField(blank=True, null=True)
+    date_of_intervention = models.DateField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.intervention_name} belonging to {self.enrollment.name}"
