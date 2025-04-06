fb209d28b6ecf8c171ebc6e146443e5f18e85ba2
Earnest Achayo <earnytechlive@gmail.com>
2025-04-06 22:40:38 +0300

    fixes database location issues

--- community_intervention_app/settings.py
+++ community_intervention_app/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
 import os
-import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,7 +68,6 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'intervention_app.db',
-        'default': dj_database_url.config(default='sqlite:///db.sqlite3'),
     }
 }
 
