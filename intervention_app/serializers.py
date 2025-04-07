1f72a7b1bdcc2c739b0bc9877f16808f378818e3
Earnest Achayo <earnytechlive@gmail.com>
2025-04-07 22:14:34 +0300

    comments the code

--- intervention_app/serializers.py
+++ intervention_app/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 from .models import Enrollment
 
-
+# Serializer for enrollment model
 class EnrollmentSerializer(serializers.ModelSerializer):
     class Meta:
+        # model to serialize
         model = Enrollment
+
+        # Include all fields
         fields = '__all__'
